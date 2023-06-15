cb0611acb2da87a0b1d1cfbbed4e24ad943934b1
vagsa <vagsa@tavrida.com>
2023-06-15 20:40:39 +0600

    add code program.cs

--- FigureSquare/Program.cs
+++ FigureSquare/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Shapes;
+
+try
+{
+    Circle circle = new (5);
+    double circleArea = circle.CalculateArea();
+    Console.WriteLine($"Площадь круга: {circleArea}");
+}
+catch (AggregateException ex)
+{
+    Console.WriteLine("Ошибка при создании треугольника" + ex.Message);
+}
+
+try
+{
+    Triangle triangle = new (3, 4, 5);
+    double triangleArea = triangle.CalculateArea();
+    Console.WriteLine($"Площадь треугольника: {triangleArea}");
+}
+catch (AggregateException ex)
+{
+    Console.WriteLine("Ошибка при создании треугольника" + ex.Message);
+}
+
+
+List<Shape> Figures = new()
+{
+    new Circle(1),
+    new Triangle(3, 3, 4),
+    new Rectangle(3, 3)
+};
+
+foreach (Shape figure in Figures)
+{
+    if (figure is Circle c)
+    {
+        Console.WriteLine($"Площадь круга: {c.CalculateArea()}\n");
+    }
+    else if (figure is Triangle t)
+    {
+        Console.WriteLine($"Площадь треугольника: {t.CalculateArea}\n");
+        Console.WriteLine($"Треугольник {(t.IsRightTriangle() ? "" : "не")}прямоугольный");
+    }
+    else if (figure is Rectangle r)
+    {
+        Console.WriteLine($"Площадь прямоугольника: {r.CalculateArea}\n");
+        Console.WriteLine($"Прямугольник - {(r.IsSquare() ? "" : "не ")}квадрат");
+    }
+}
