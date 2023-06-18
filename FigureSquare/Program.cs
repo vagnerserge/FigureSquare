24b8028b89c8ae8a18caee4eb5890acc8ac8b087
vagsa <vagsa@tavrida.com>
2023-06-18 11:13:12 +0600

    add input of data fom user

--- FigureSquare/Program.cs
+++ FigureSquare/Program.cs
@@ -1,25 +1,54 @@
 ﻿using Shapes;
 
 Console.WriteLine("------------Создание фигур и вычисление площади фигуры в try-catch-----------------------");
-try
+
+Console.WriteLine("Введите радиус круга:");
+string? radiusInput = Console.ReadLine();
+
+if (double.TryParse(radiusInput, out double radius))
 {
-    Circle circle = new (5);
-    Console.WriteLine($"Площадь круга: {circle.CalculateArea():F3}");
+    try
+    {
+        Circle circle = new(radius);
+        Console.WriteLine($"Площадь круга: {circle.CalculateArea():0.###}");
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine("Ошибка при создании треугольника: " + ex.Message);
+    }
 }
-catch (ArgumentException ex)
+else
 {
-    Console.WriteLine("Ошибка при создании треугольника" + ex.Message);
+    Console.WriteLine("Некорректный ввод радиуса круга.");
 }
 
-try
+
+    Console.WriteLine("Введите сторону A треугольника:");
+    string? sideAInput = Console.ReadLine();
+    Console.WriteLine("Введите сторону B треугольника:");
+    string? sideBInput = Console.ReadLine();
+    Console.WriteLine("Введите сторону C треугольника:");
+    string? sideCInput = Console.ReadLine();
+
+if (double.TryParse(sideAInput, out double sideA) &&
+    double.TryParse(sideBInput, out double sideB) &&
+    double.TryParse(sideCInput, out double sideC))
 {
-    Triangle triangle = new (3, 4, 5);
-    Console.WriteLine($"Площадь треугольника: {triangle.CalculateArea():F3}");
+    try
+    {
+        Triangle triangle = new(sideA, sideB, sideC);
+        Console.WriteLine($"Площадь треугольника: {triangle.CalculateArea():0.###}");
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine("Ошибка при создании треугольника: " + ex.Message);
+    }
 }
-catch (ArgumentException ex)
+else
 {
-    Console.WriteLine("Ошибка при создании треугольника" + ex.Message);
+    Console.WriteLine("Некорректный ввод сторон треугольника.");
 }
+
 Console.WriteLine("------------------------------------------------------------------------------------------\n");
 
 Console.WriteLine("------------Вычисление площади фигуры без знания типа фигуры в compile-time----------------");
@@ -35,17 +64,16 @@ foreach (Shape figure in Figures)
 {
     if (figure is Circle c)
     {
-        ;
-        Console.WriteLine($"Площадь круга: {c.CalculateArea()}\n");
+        Console.WriteLine($"Площадь круга: {c.CalculateArea():0.###}\n");
     }
     else if (figure is Triangle t)
     {
-        Console.WriteLine($"Площадь треугольника: {t.CalculateArea()}");
+        Console.WriteLine($"Площадь треугольника: {t.CalculateArea():0.###}");
         Console.WriteLine($"Треугольник {(t.IsRightTriangle() ? "" : "не")}прямоугольный\n");
     }
     else if (figure is Rectangle r)
     {
-        Console.WriteLine($"Площадь прямоугольника: {r.CalculateArea()}");
+        Console.WriteLine($"Площадь прямоугольника: {r.CalculateArea():0.###}");
         Console.WriteLine($"Прямугольник - {(r.IsSquare() ? "" : "не ")}квадрат");
     }
 }
